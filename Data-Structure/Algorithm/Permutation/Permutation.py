258144dbb0770e2e044ab8a3b4c3dd3a057184de
tz70s <su3g4284zo6y7@gmail.com>
2015-08-27 22:46:24 +0800

    modify swap

--- Data-Structure/Algorithm/Permutation/Permutation.py
+++ Data-Structure/Algorithm/Permutation/Permutation.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python3
-
 def Permutation(input_list,start,end):
     if start == end :
         print (input_list)
     else:
         for j in range(start,end):
-            temp = input_list[start]
-            input_list[start] = input_list[j]
-            input_list[j] = temp
+            input_list[start],input_list[j] = input_list[j],input_list[start]
             Permutation(input_list,start+1,end)
-            temp = input_list[start]
-            input_list[start] = input_list[j]
-            input_list[j] = temp
+            input_list[start],input_list[j] = input_list[j],input_list[start]
 
 newList = list("abcd")
 Permutation(newList,0,4)
--- Data-Structure/Algorithm/Selection-Sort/SelectionSort.py
+++ Data-Structure/Algorithm/Selection-Sort/SelectionSort.py
@@ -5,9 +5,7 @@ def selectionSort(input_list):
         for j in range(i,len(input_list)):
             if input_list[j]<input_list[minn]:
                 minn = j
-        temp = input_list[i]
-        input_list[i] = input_list[minn]
-        input_list[minn] = temp
+        input_list[i],input_list[minn] = input_list[minn],input_list[i]
     return input_list
 
 newList = [1,24,3,2,7,5,8,3]
