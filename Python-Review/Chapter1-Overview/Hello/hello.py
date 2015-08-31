fdcee4a3f7dce398db1885d4ceacf0f48525fb7f
tz70s <su3g4284zo6y7@gmail.com>
2015-08-31 22:50:54 +0800

    remove garbage

--- Python-Review/Chapter1-Overview/Hello/hello.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-#chmod +x hello.py
-#It will insert space and newline in preset
-print ("Hello", "World!")
--- Python-Review/Chapter1-Overview/SimpleException/except.py
+++
@@ -1,8 +0,0 @@
-#! usr/bin/env python3
-
-s = input("Enter an integer\n")
-try:
-    i = int(s)
-    print("Valid integer!")
-except ValueError as err:
-    print(err)
