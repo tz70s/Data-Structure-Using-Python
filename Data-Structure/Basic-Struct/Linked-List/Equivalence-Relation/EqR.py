0e393866e91ebecc42c3d1dfd4c33215e0388ad9
tz70s <su3g4284zo6y7@gmail.com>
2015-09-03 10:07:29 +0800

    add Stack and Queue

--- Data-Structure/Basic-Struct/Linked-List/Equivalence-Relation/EqR.py
+++ Data-Structure/Basic-Struct/Linked-List/Equivalence-Relation/EqR.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python3
 
 import linklist
+import stack
+
+class CreateError(Exception):pass
 
 
-if __name__ == "__main__":
-    myList = linklist.UnorderedList()
+def main():
+    try:
+        myList = linklist.UnorderedList()
+        myStack = stack.Stack()
+    except CreateError as err:
+        print (err)
+
     myList.insert(5)
     myList.insert(6)
     myList.reveal()
+    myStack.push(5)
+    myStack.push(6)
+    print(myStack.reveal())
+
+
+
+if __name__ == "__main__":
+    main()
