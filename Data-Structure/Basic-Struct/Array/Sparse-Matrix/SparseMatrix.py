ec18a823587afe376a0d9c8369fbaa3de61a8cdf
tz70s <su3g4284zo6y7@gmail.com>
2015-09-05 11:15:56 +0800

    add sparse matrix transpose

--- Data-Structure/Basic-Struct/Array/Sparse-Matrix/SparseMatrix.py
+++ Data-Structure/Basic-Struct/Array/Sparse-Matrix/SparseMatrix.py
@@ -1,25 +1,48 @@
+#!/usr/bin/env python3
+
 class Matrix:
     def __init__(self, row=0, col = 0, value = 0):
         self.row = row
         self.col = col
         self.value = value
+
     def getrow(self):
         return self.row
+
     def getcol(self):
         return self.col
+
     def getvalue(self):
-        return self.getvalue
+        return self.value
 
 
-if "__name__" == "__main__":
-    #init Matrix
+def matInit():
     matlist = []
-    for row , col in range(10),range(10):
-        matlist.append(Matrix(row,col,row*col))
+    spmatlist = []
+    for line in open("mat.txt"):
+        matlist.append(line.split())
+    for row in range(len(matlist)):
+        for col in range(len(matlist)):
+            if int(matlist[row][col]) != 0:
+                spmatlist.append(Matrix(row,col,int(matlist[row][col])))
+            else:
+                continue
+    for n in range(len(spmatlist)):
+        print ("row: {0}, col: {1}, value: {2}".format(spmatlist[n].row,spmatlist[n].col,spmatlist[n].value))
 
-    for row in range(10):
-        line = []
-        for col in range(10):
-            line.append(matlist)
+    del matlist
+    Transpose(spmatlist)
+    
+
+def Transpose(spmat):
+    # row -> col , col -> row
+    for n in spmat:
+        n.row , n.col = n.col , n.row
+    for n in spmat:
+        print ("row: {0}, col: {1}, value: {2}".format(n.row,n.col,n.value))
 
 
+
+if __name__ == "__main__":
+    matInit()
+    
