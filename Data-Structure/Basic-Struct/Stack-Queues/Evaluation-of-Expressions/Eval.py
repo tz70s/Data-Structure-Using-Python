f8e1677c950b3adae5c1882049908c3ce5aebe26
tz70s <su3g4284zo6y7@gmail.com>
2015-09-06 14:44:57 +0800

    Update Eval.py

--- Data-Structure/Basic-Struct/Stack-Queues/Evaluation-of-Expressions/Eval.py
+++ Data-Structure/Basic-Struct/Stack-Queues/Evaluation-of-Expressions/Eval.py
@@ -4,29 +4,29 @@ import queue
 
 opset = {'*','+','(',")"}
 
-
 def readinfix():
-	fh = open("infix.txt")
-	li = []
-	li += fh.read()
-	print (li)
-	return li
+    fh = open("infix.txt")
+    li = []
+    li += fh.read()
+    print (li)
+    return li
 
 def eval():
-	evlist = readinfix()
-	postlist = []
-	st = stack.Stack()
+    evlist = readinfix()
+    postlist = []
+    st = stack.Stack()
+    for op in evlist:
+        if op in opset:
+            st.push(op)
+        else:
+            postlist.append(op)
 
-	for op in evlist:
-		if op in opset:
-			st.push(op)
-		else:
-			postlist.append(op)
-			if st.isEmpty:
-				print (op)
-			else:
-				print (st.pop())
+            if st.isEmpty:
+                continue
+            else:
+                postlist.append(st.pop())
 
+    print (postlist)
 
 if __name__ == "__main__":
-	eval()
+    eval()
